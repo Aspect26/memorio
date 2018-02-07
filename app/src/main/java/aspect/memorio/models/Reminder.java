2d227fc49510a98e9c62485435725c1865b5de68
Julius Flimmel <flimmelj@gmail.com>
2018-02-07 11:26:24 +0100

    Added id for reminders

--- app/src/main/java/aspect/memorio/models/Reminder.java
+++ app/src/main/java/aspect/memorio/models/Reminder.java
@@ -4,10 +4,13 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class Reminder {
 
     private static final DateFormat DATE_FORMAT = new SimpleDateFormat("y-MMMM-d-H-m-s");
+
+    private String id;
 
     private String text;
 
@@ -15,14 +18,27 @@ public class Reminder {
 
     private Date notificationDate;
 
-    public Reminder(String text, Date date, Date notificationDate) {
+    private Reminder(final String id, final String text, final Date date, final Date notificationDate) {
+        this.id = id;
         this.text = text;
         this.date = date;
         this.notificationDate = notificationDate;
     }
 
+    public Reminder(String text, Date date, Date notificationDate) {
+        this(UUID.randomUUID().toString(), text, date, notificationDate);
+    }
+
     public Reminder() {
         this("", null, null);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getText() {
@@ -55,25 +71,26 @@ public class Reminder {
 
         stringBuilder.append(this.getText()).append(";");
         stringBuilder.append((this.getDate() != null)? DATE_FORMAT.format(this.getDate()) : "").append(";");
-        stringBuilder.append((this.getNotificationDate() != null)? DATE_FORMAT.format(this.getNotificationDate()) : "");
+        stringBuilder.append((this.getNotificationDate() != null)? DATE_FORMAT.format(this.getNotificationDate()) : "").append(";");
+        stringBuilder.append(this.getId());
 
         return stringBuilder.toString();
     }
 
     public static Reminder createFromString(String line) throws ParseException {
         String[] data = line.split(";", -1);
-        if (data.length != 3) {
-            return null;
-        } else {
-            String text = data[0];
-            String dateText = data[1];
-            String notificationDateText = data[2];
+        String text = data[0];
+        String dateText = data[1];
+        String notificationDateText = data[2];
+        String id = (data.length > 3)? data[3] : "";
 
-            Date date = (dateText != null && dateText.length() > 0)? DATE_FORMAT.parse(dateText) : null;
-            Date notificationDate = (notificationDateText != null && notificationDateText.length() > 0)? DATE_FORMAT.parse(notificationDateText) : null;
-
-            return new Reminder(text, date, notificationDate);
+        Date date = (dateText != null && dateText.length() > 0)? DATE_FORMAT.parse(dateText) : null;
+        Date notificationDate = (notificationDateText != null && notificationDateText.length() > 0)? DATE_FORMAT.parse(notificationDateText) : null;
+        if (id.isEmpty()) {
+            id = UUID.randomUUID().toString();
         }
+
+        return new Reminder(id, text, date, notificationDate);
     }
 
 }
