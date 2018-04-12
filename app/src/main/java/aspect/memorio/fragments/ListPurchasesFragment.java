02ed5ba8482fbeb55e81f3089d69475c10f13270
Julius Flimmel <flimmelj@gmail.com>
2018-04-12 11:59:46 +0200

    Added undo for item purchasing
    Closes #52

--- app/src/main/java/aspect/memorio/fragments/ListPurchasesFragment.java
+++ app/src/main/java/aspect/memorio/fragments/ListPurchasesFragment.java
@@ -1,8 +1,12 @@
 package aspect.memorio.fragments;
 
+import android.view.View;
+
+import aspect.memorio.R;
 import aspect.memorio.fragments.config.PurchasesFragmentConfig;
 import aspect.memorio.models.Purchase;
 import aspect.memorio.storage.PurchasesStorage;
+import aspect.memorio.utils.SnackbarUtils;
 
 public class ListPurchasesFragment extends ListFragment<Purchase> {
 
@@ -14,10 +18,21 @@ public class ListPurchasesFragment extends ListFragment<Purchase> {
         return this.homeActivity.getPurchasesStorage();
     }
 
-    public void buyPurchase(Purchase purchase) {
+    public void buyPurchase(final Purchase purchase) {
         purchase.setBought(true);
         this.reinitializeItemsView();
         this.getStorage().flushAll();
+
+        if (getView() != null) {
+            SnackbarUtils.showUndoSnackbar(getView(), R.string.snackbar_purchase_bought, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    purchase.setBought(false);
+                    storage.updateOrAdd(purchase);
+                    reinitializeItemsView();
+                }
+            });
+        }
     }
 
 }
